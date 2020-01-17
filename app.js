0ea8db5b5d9bc219ff678b44c0122400ab9a653f
Jarmo Kärnä <Jarmo.Karna@seamk.fi>
2020-01-17 17:29:41 +0200

    .

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ app.get('/', (req, res, next) => {
     //res.end();
 });
 
+app.get('/', (req, res, next) => {
+    res.send('Hello world3');
+    //res.write
+    //res.end();
+});
+
+
+
 app.use((req, res, next) => {
     res.status(404);
     res.send(`
