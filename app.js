8d171a8d8cccc14df2d7e89113403ba43003a24e
Jarmo Kärnä <Jarmo.Karna@seamk.fi>
2020-01-17 17:20:02 +0200

    .

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const PORT = process.env.PORT || 8080; //jos porttia ei ole asetettu ympäristömuuttujaan, käytä porttia 8080
+let app = express();
+
+//app.use(); kuunnellaan kaikkia sisääntulevia viestejä
+//app.get(); 
+//app.post();
+
+app.use((req, res, next) => { //next parametri on "jatka seuraavaankin kuuntelijaan"
+    console.log(`path: ${req.path}`);
+    next(); // "seuraava kuuntelija"
+});
+
+
+
+
+app.get('/', (req, res, next) => {
+    res.send('Hello world');
+    //res.write
+    //res.end();
+});
+
+app.use((req, res, next) => {
+    res.status(404);
+    res.send(`
+    page not found`)
+
+
+})
+
+
+app.listen(PORT);
